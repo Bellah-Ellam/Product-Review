f30d7c9eff473f0605feccacbb1cfe25106e6e60
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-06-05 09:09:21 +0300

    added foreign keys

--- db/migrate/20230604183917_create_reviews.rb
+++ db/migrate/20230604183917_create_reviews.rb
@@ -1,9 +1,11 @@
-class CreateReviews < ActiveRecord::Migration[6.1]
+class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.integer :user_id
       t.integer :product_id
       t.string :content
+      t.belongs_to :product, foreign_key: true
+      t.belongs_to :user, foreign_key: true
     end
   end
 end
